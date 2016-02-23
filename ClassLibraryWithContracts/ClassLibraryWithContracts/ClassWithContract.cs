154bc795b7e9ac4bcd0cc55ae9bba436bfbce87e
David Rovani <david@rovani.net>
2016-02-23 11:42:07 -0600

    Initial commit of a bare minimum two-project solution that includes a method with Contract.Requires and Contract.Requires<TException>. Assembly Mode set to "Standard Contract Requires", and Runtime Checking is set to "Preconditions".

---
+++ ClassLibraryWithContracts/ClassLibraryWithContracts/ClassWithContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace ClassLibraryWithContracts
+{
+    public class ClassWithContract
+    {
+        public string RequiresNonNullString(string arg)
+        {
+            Contract.Requires(arg != null);
+
+            return arg;
+        }
+        public string RequiresNonNullStringGeneric(string arg)
+        {
+            Contract.Requires<ArgumentNullException>(arg != null);
+
+            return arg;
+        }
+    }
+}
---
+++ ClassLibraryWithContracts/ClassLibraryWithContractsTests/ClassWithContractTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using Xunit;
+
+namespace ClassLibraryWithContracts.Tests
+{
+    public class ClassWithContractTests
+    {
+        public ClassWithContractTests()
+        {
+            Contract.ContractFailed += (sender, e) =>
+            {
+                e.SetHandled();
+                e.SetUnwind();
+
+                throw new Exception(e.FailureKind + ": " + e.Message);
+            };
+        }
+
+        [Fact]
+        public void RequiresNonNullString_When_Passed_Null_Throws_ArgumentNullException()
+        {
+            var ctr = new ClassWithContract();
+            Assert.ThrowsAny<Exception>(() => ctr.RequiresNonNullString(null));
+        }
+
+        [Fact]
+        public void RequiresNonNullString_Returns_String_When_NotEmpty()
+        {
+            var ctr = new ClassWithContract();
+            string ret = ctr.RequiresNonNullString("TestString");
+
+            Assert.Equal("TestString", ret);
+        }
+        [Fact]
+        public void RequiresNonNullStringGeneric_When_Passed_Null_Throws_ArgumentNullException()
+        {
+            var ctr = new ClassWithContract();
+            Assert.ThrowsAny<Exception>(() => ctr.RequiresNonNullStringGeneric(null));
+        }
+
+        [Fact]
+        public void RequiresNonNullStringGeneric_Returns_String_When_NotEmpty()
+        {
+            var ctr = new ClassWithContract();
+            string ret = ctr.RequiresNonNullStringGeneric("TestString");
+
+            Assert.Equal("TestString", ret);
+        }
+    }
+}
