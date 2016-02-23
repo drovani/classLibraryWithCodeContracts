76c45e245d3a1bf5a78bb0c32d60ae7ccc2157d4
David Rovani <david@rovani.net>
2016-02-23 12:53:56 -0600

    Added Contract.Ensures calls, just to see if those will trigger anything.

--- ClassLibraryWithContracts/ClassLibraryWithContracts/ClassWithContract.cs
+++ ClassLibraryWithContracts/ClassLibraryWithContracts/ClassWithContract.cs
@@ -8,12 +8,14 @@ namespace ClassLibraryWithContracts
         public string RequiresNonNullString(string arg)
         {
             Contract.Requires(arg != null);
+            Contract.Ensures(Contract.Result<string>() == arg);
 
             return arg;
         }
         public string RequiresNonNullStringGeneric(string arg)
         {
             Contract.Requires<ArgumentNullException>(arg != null);
+            Contract.Ensures(Contract.Result<string>() == arg);
 
             return arg;
         }
