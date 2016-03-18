da50bec463b5f5b72e99f624bff44471acb09369
David Rovani <david@rovani.net>
2016-03-18 16:13:46 -0500

    Attempting to use the DotNet.Contracts NuGet package instead of the official System.Diagnostics.Contracts package.

--- ClassLibraryWithContracts/ClassLibraryWithContractsTests/ClassWithContractTests.cs
+++ ClassLibraryWithContracts/ClassLibraryWithContractsTests/ClassWithContractTests.cs
@@ -32,14 +32,16 @@ namespace ClassLibraryWithContracts.Tests
 
             Assert.Equal("TestString", ret);
         }
-        [Fact(Skip = "Tests Contract.Requires<TException>, which requires CCRewrite on the build server.")]
+        [Fact]
+        //[Fact(Skip = "Tests Contract.Requires<TException>, which requires CCRewrite on the build server.")]
         public void RequiresNonNullStringGeneric_When_Passed_Null_Throws_ArgumentNullException()
         {
             var ctr = new ClassWithContract();
             Assert.ThrowsAny<Exception>(() => ctr.RequiresNonNullStringGeneric(null));
         }
 
-        [Fact(Skip = "Tests Contract.Requires<TException>, which requires CCRewrite on the build server.")]
+        [Fact]
+        //[Fact(Skip = "Tests Contract.Requires<TException>, which requires CCRewrite on the build server.")]
         public void RequiresNonNullStringGeneric_Returns_String_When_NotEmpty()
         {
             var ctr = new ClassWithContract();
